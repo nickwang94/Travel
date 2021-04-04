5abda6064ce1a74faed14542ee1584f53bc9f90e
wangwk-a <wangwk-a@glodon.com>
2021-04-04 17:21:21 +0800

    完成周末去哪儿的功能

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="recommand-title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommandList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" alt="item.title">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommandList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/19/58f233e11df7ef.jpg_r_640x214_6b42c9fc.jpg',
+          title: '西安必打卡',
+          desc: '寻长安梦，开启穿越的圆梦之旅'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1811/63/808c022b285a2c.jpg_r_640x214_ad984479.jpg',
+          title: '探寻文化古都',
+          desc: '一场时空穿越的旅行，探寻历史悠久的文化古都！寻觅历史留下的点点足迹，细细品味古人的伟大'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/30/67d5e62ca78a29.jpg_r_640x214_9dfb4295.jpg',
+          title: '西安花花世界',
+          desc: '月落星驰又一春，不负人间看花人'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
+  .recommand-title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+    text-align: left
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 33.9%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+</style>
+
